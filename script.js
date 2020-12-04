c3af8092c8e544d3dbf34bfc309c0d6116b27fe3
samuel-assemien <assemiensamuel48@gmail.com>
2020-12-04 19:10:12 +0000

    Edition du contenu du fichier
    	- requete ajax
    	- implementation de la fonction smprintf

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 (function () {
-    console.log(smprintf(`<div>
-            slaut a tous aujourd'hui est le %s
-        </div>`,`%s`,new Date()))
+    const div = document.createElement(`div`)
+    const divBody = `
+    Salut a tous moi c'est Samy Developpeur Web FullStack
+    Mon nom d'utilisateur sur Github : <strong>%s</strong>.<br>
+    L'email au quel vous pouvez me contater : <strong>%s</strong>.<br>
+    Ainsi que mon contact Whatssap : +225 01937964 <br>
+    En effet cet texte a été formater par la fonction <em>sm-printf</em><br>
+    Du package : <strong>%s</strong> . Et a besoin du fichier composer.json pour fonctioner<br>
+    convenablement !`
+    const container = document.querySelector(".mt-5")
+    const xhr = new XMLHttpRequest()
+    xhr.open(`get`, `composer.json`)
+    xhr.responseType = "json"
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const composer = xhr.response
+            container.innerHTML = smprintf(divBody, `%s`,
+                composer.authors[0].name, composer.authors[0].email, composer.name)
+        }
+    }
+    xhr.send()
 })()
